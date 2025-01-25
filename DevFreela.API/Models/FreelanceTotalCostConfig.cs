570b3fc5ae8d028500cf299834221ef8601bfcba
Leanderson Lima <leanderson.devlima@gmail.com>
2025-01-25 15:05:04 -0300

    Add project files.

---
+++ DevFreela.API/Models/FreelanceTotalCostConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Models
+{
+    public class FreelanceTotalCostConfig : ControllerBase
+    {
+        public decimal Minimum { get; set; }
+        public decimal Maximum { get; set; }
+
+    }
+}
---
+++ DevFreela.API/Models/UpdateProjectInputModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Models
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpdateProjectInputModel : ControllerBase
+    {
+        public int IdProject { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int IdFrelancer { get; set; }
+        public decimal TotalCost { get; set; }
+    }
+}
+
